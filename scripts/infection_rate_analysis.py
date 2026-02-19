c03a26e0249495fb661ac3121dd5c91f7719b854
vilas <vilaskr2962@gmail.com>
2026-02-19 16:31:55 +0530

    Add Program: [ Vilas ] desplayed top 10 countries by infection rate

--- scripts/infection_rate_analysis.py
+++ scripts/infection_rate_analysis.py
@@ -11,7 +11,8 @@
 # -------------------------------------------------------------------------
 
 from pyspark.sql import SparkSession
-from pyspark.functions import * 
+from pyspark.functions import *
+from pyspark.sql.window import Window 
 
 # -------------------------------------------------------------------------
 # 1. Spark Session
@@ -34,18 +35,28 @@ df_worldometer_data = spark.read.parquet(STAGING_PATH + \
 # 3. Confirmed cases per 1000 population
 # -------------------------------------------------------------------------
 df_worldometer_data = df_worldometer_data.withColumn(
-    'confirmed_cases_per(1000)',
-    round(
-            (col('total_cases') / col('population')) * 1000, 2
-        )
+    'confirmed_cases_1000',
+    round((col('total_cases') / col('population')) * 1000, 2)
     )
 
 # -------------------------------------------------------------------------
 # 4. Active cases per 1000 population
 # -------------------------------------------------------------------------
 df_worldometer_data = df_worldometer_data.withColumn(
-    'active_cases_per(1000)',
-    round(
-            (col('active_cases') / col('population')) * 1000, 2
-        )
+    'active_cases_1000',
+    round((col('active_cases') / col('population')) * 1000, 2)
     )
+
+# -------------------------------------------------------------------------
+# 5. Top 10 countries by infection rate
+# -------------------------------------------------------------------------
+df_worldometer_data.sort(col('confirmed_cases_1000').desc()) \
+    .select(col('country_region').alias('country'), 
+            col('confirmed_cases_1000').alias('infection_rate')
+        ).show(10)
+
+print('''
+---------------------------------------------------------------------------
+Top 10 infected countries
+''')
+
