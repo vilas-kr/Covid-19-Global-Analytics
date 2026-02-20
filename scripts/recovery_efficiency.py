da4f15b3d935b063e6816bbd9ff0cae13dbf4bd5
vilas <vilaskr2962@gmail.com>
2026-02-20 13:59:21 +0530

    Add Program : [ Vilas ] calculated recovered percentage per country

---
+++ scripts/recovery_efficiency.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# -------------------------------------------------------------------------
+# Task 5: Recovery Efficiency
+
+# Recovered percentage per country.
+# 7-day rolling recovery average (Window function).
+# Country with fastest recovery growth.
+# Peak recovery day per country
+# -------------------------------------------------------------------------
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.window import Window
+
+# -------------------------------------------------------------------------
+# 1. Spark Session
+# -------------------------------------------------------------------------
+spark = SparkSession.builder \
+    .appName('Recovery Analysis') \
+    .getOrCreate()
+    
+spark.sparkContext.setLogLevel('ERROR')
+
+STAGING_PATH = 'hdfs:///data/covid/staging/'
+ANALYTICS_PATH = 'hdfs:///data/covid/analytics/'
+
+# -------------------------------------------------------------------------
+# 2. Read dataset from hadoop 
+# -------------------------------------------------------------------------
+df_full_grouped = spark.read.parquet(STAGING_PATH + 'full_grouped_parquet')
+df_day_wise = spark.read.parquet(STAGING_PATH + 'day_wise_parquet')
+
+# -------------------------------------------------------------------------
+# 3. Recovered percentage per country
+# -------------------------------------------------------------------------
+country_recovered_percentage = df_full_grouped.groupBy(
+     col('country_region')
+    ).agg(
+        sum(col('confirmed')).alias('total_confirmed'),
+        sum(col('recovered')).alias('total_recovered')
+    ).withColumn('recovered_percentage'
+        when(col('total_confirmed') > 0, 
+             round((col('total_recovered') / col('total_confirmed')) * 100, 
+                2)
+        ).otherwise(0)
+    )
+
+
+
